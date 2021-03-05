69e4d6111fa08afd071f6a02aec0d6432f4fb69b
Froilan-examen <fenriquez31@cepnet.net>
2021-03-05 13:38:14 +0100

    Add tipus incidencia and question relation to incidencies model

--- broggi/app/Models/Incidencies.php
+++ broggi/app/Models/Incidencies.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Incidencies extends Model
 {
     use HasFactory;
+
+    protected $table = 'incidencies';
+    public $timestamps = false;
+
+
+    public function tipus_incidencies(){
+        return $this->belongsTo(Tipus_incidencias::class, 'tipus_incidencies_id');
+    }
+
+    public function questions(){
+        return $this->hasMany(Question::class, 'incidencies_id');
+    }
+
+
 }
