348961e8f14b7d7222ab39d222fcdecad6fe1a8c
Froilan-examen <fenriquez31@cepnet.net>
2021-03-05 12:51:35 +0100

    Create Incidencies model and controller

---
+++ broggi/app/Http/Controllers/IncidenciesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Incidencies;
+use Illuminate\Http\Request;
+
+class IncidenciesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Incidencies  $incidencies
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Incidencies $incidencies)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Incidencies  $incidencies
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Incidencies $incidencies)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Incidencies  $incidencies
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Incidencies $incidencies)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Incidencies  $incidencies
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Incidencies $incidencies)
+    {
+        //
+    }
+}
