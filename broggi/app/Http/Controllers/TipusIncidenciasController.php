abaa845109964bc77f71dfcbc9d42e42df363802
Froilan-examen <fenriquez31@cepnet.net>
2021-03-05 12:54:28 +0100

    Create Tipus_incidencias model and controller

---
+++ broggi/app/Http/Controllers/TipusIncidenciasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tipus_incidencias;
+use Illuminate\Http\Request;
+
+class TipusIncidenciasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tipus_incidencias  $tipus_incidencias
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tipus_incidencias $tipus_incidencias)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tipus_incidencias  $tipus_incidencias
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tipus_incidencias $tipus_incidencias)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tipus_incidencias  $tipus_incidencias
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tipus_incidencias $tipus_incidencias)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tipus_incidencias  $tipus_incidencias
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tipus_incidencias $tipus_incidencias)
+    {
+        //
+    }
+}
---
+++ broggi/app/Models/Tipus_incidencias.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tipus_incidencias extends Model
+{
+    use HasFactory;
+}
