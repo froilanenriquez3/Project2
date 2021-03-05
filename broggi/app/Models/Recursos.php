bc6c31148508fb6037adb91aa70a191ff96d52f8
Froilan-examen <fenriquez31@cepnet.net>
2021-03-05 13:54:55 +0100

    Add relations to Recursos model

--- broggi/app/Models/Recursos.php
+++ broggi/app/Models/Recursos.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Recursos extends Model
 {
     use HasFactory;
+
+    protected $table = 'recursos';
+    public $timestamps = false;
+
+    public function tipus_recursos(){
+        return $this->belongsTo(Tipus_recursos::class, 'tipus_recursos_id');
+    }
+
+    public function incidencies_has_recursos(){
+        return $this->belongsToMany(Recursos::class, 'incidencies_has_recursos',  'recursos_id', 'incidencies_id')->withPivot('id');
+    }
+
 }
