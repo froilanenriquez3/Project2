8503ac97c61666043eff001d03e2f2513d69ad19
Mireia <misala91@gmail.com>
2021-03-12 11:29:33 +0100

    estructura Navbar

---
+++ broggi/resources/views/partials/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    </a>
+    <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Nueva Incidencia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ver incidencias</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Alertantes Habituales</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Recursos moviles</a>
+        </li>
+      </ul>
+  </nav>
