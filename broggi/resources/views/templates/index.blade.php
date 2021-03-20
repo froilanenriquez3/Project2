83bca9b0aca20e20650141e0356d341e25961ed3
Froilan-examen <fenriquez31@cepnet.net>
2021-03-20 20:33:45 +0100

    Revised create template layout

--- broggi/resources/views/templates/index.blade.php
+++ broggi/resources/views/templates/index.blade.php
@@ -1,18 +1,51 @@
 {{-- Page template for index of each table page --}}
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- Bootstrap cyborg --}}
+    <link rel="stylesheet" href=" {{ asset('../resources/css/bootstrap.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('../resources/css/indexTemplate.css?' . rand()) }}">
+
     <title>@yield('title')</title>
 </head>
+
 <body>
+    <header>
+        <nav>
+            {{-- import for navbar --}}
+            <p>temporary</p>
+        </nav>
+    </header>
 
-    <p>I am the template</p>
+    <aside>
+        {{-- import for sidebar --}}
+        <p>temporary</p>
+    </aside>
 
-    @yield('content')
+    <main>
+
+        <section id="formSection">
+            <div id="divForm">
+                @yield('form')
+            </div>
+
+        </section>
 
 
+    </main>
+
+    <script src="{{ url('https://code.jquery.com/jquery-3.5.1.slim.min.js') }}"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="{{ url('https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js') }}"
+        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
