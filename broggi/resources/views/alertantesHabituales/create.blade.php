d9308b4a4e062f5c05526fa60b4b8057fe99dacb
Froilan-examen <fenriquez31@cepnet.net>
2021-03-22 09:04:40 +0100

    Changes for template for create and index

--- broggi/resources/views/alertantesHabituales/create.blade.php
+++ broggi/resources/views/alertantesHabituales/create.blade.php
@@ -1,5 +1,35 @@
 @extends('templates.create')
+@section('links')
+    <link rel="stylesheet" href=" {{ url('../resources/css/alertants/alertantsCreate.css?'.rand()) }}">
+@endsection
 @section('title','Alertantes Habituales Formulario')
 @section('form')
+
+<h5 id="formTitle">Alertants</h5>
+
+
+<form action="" id="form">
+
+    <div class="form-group row">
+        <label class="col-2" for="alertantTelefon">Telefon</label>
+        <input class="col-10" type="text" id="alertantTelefon" name="alertantTelefon">
+    </div>
+    <div class="form-group row">
+        <label class="col-2" for="alertantNom">Nom</label>
+        <input class="col-10" type="text" id="alertantNom" name="alertantNom">
+    </div>
+    <div class="form-group row">
+        <label class="col-2" for="alertantCognom">Cognoms</label>
+        <input class="col-10" type="text" id="alertantCognom" name="alertantCognom">
+    </div>
+    <div class="form-group row">
+        <label class="col-2" for="alertantAdreca">Adreca</label>
+        <input class="col-10" type="number" id="alertantAdreca" name="alertantAdreca">
+
+    </div>
+
+
+</form>
+
 
 @endsection
