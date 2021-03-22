1c5bcc20f8825904e9c25cf9cbf6fe7e07a0823b
Froilan-examen <fenriquez31@cepnet.net>
2021-03-22 13:58:34 +0100

    create home page of teleoperator

--- broggi/resources/views/homePages/home.blade.php
+++ broggi/resources/views/homePages/home.blade.php
@@ -1,1 +1,30 @@
-<p>Home page</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- Bootstrap default --}}
+    <link rel="stylesheet" href="{{ url('https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css') }}" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="{{ asset('../resources/css/homePages/home.css?' . rand()) }}">
+    @yield('links')
+    <title>@yield('title')</title>
+</head>
+<body>
+
+    <main>
+        <header>
+            <nav>
+                {{-- import for navbar --}}
+                <p>temporary</p>
+            </nav>
+        </header>
+
+        @yield('content')
+    </main>
+
+</body>
+</html>
---
+++ broggi/resources/views/homePages/teleoperador.blade.php
@@ -0,0 +1,13 @@
+@extends('homePages.home')
+@section('links')
+    <link rel="stylesheet" href="{{ asset('../resources/css/homePages/teleoperador.css?' . rand()) }}">
+@endsection
+@section('title','Teleoperador')
+@section('content')
+    <section id="menuSection">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </section>
+@endsection
