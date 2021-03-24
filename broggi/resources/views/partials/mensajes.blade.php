1057158d861a49184c89bde324329e0905d97b30
Froilan-examen <fenriquez31@cepnet.net>
2021-03-24 10:49:25 +0100

    Modify bootswatch

---
+++ broggi/resources/views/partials/mensajes.blade.php
@@ -0,0 +1,21 @@
+@if (Session::has('error'))
+
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    {{ Session::get('error') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+
+@endif
+
+@if( Session::has('mensaje'))
+
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ Session::get('mensaje') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+
+@endif
