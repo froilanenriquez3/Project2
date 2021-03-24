e838bc0a76857abd5c22384049852a8ddb085f97
Froilan-examen <fenriquez31@cepnet.net>
2021-03-24 10:32:29 +0100

    Modify login, configure Vue and bootstrap

--- broggi/app/Http/Controllers/UsuarisController.php
+++ broggi/app/Http/Controllers/UsuarisController.php
@@ -3,12 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Models\Usuaris;
+use App\Clases\Utilitat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\QueryException;
 
 class UsuarisController extends Controller
 {
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logIn(Request $request){
+        // $route = view('login');
+        $username = $request->input('username');
+
+        $user = Usuaris::where('username', $username)->get()->first();
+        if(!empty($user)){
+            $password = $request->input('contrasenya');
+            if(Hash::check($password, $user->contrasenya)){
+                Auth::login($user);
+                $route = view('homePages.home');
+            }
+
+        }
+        return $route;
+    }
+
     /**
      * Display a listing of the resource.
      *
