22d6a8139850d614aa395671787679f9f669584a
Froilan-examen <fenriquez31@cepnet.net>
2021-03-29 12:08:00 +0200

    Change navbar depending on type of user Alertants page

--- broggi/resources/views/alertantesHabituales/createvue.blade.php
+++ broggi/resources/views/alertantesHabituales/createvue.blade.php
@@ -4,10 +4,16 @@
 @endsection
 @section('title','Alertantes Habituales Formulario')
 
-{{-- Add if for admin navbar --}}
-{{-- Navbar teleoperador --}}
 @section('navbar')
-@include('partials.navbarTeleoperador')
+{{-- Add if for admin navbar --}}
+@if(Auth::user()->rols_id == 1)
+    {{-- Navbar admin --}}
+    @include('partials.navbarAdmin')
+@elseif(Auth::user()->rols_id == 2)
+{{-- Navbar teleoperador --}}
+    @include('partials.navbarTeleoperador')
+@endif
+
 @endsection
 
 @section('form')
