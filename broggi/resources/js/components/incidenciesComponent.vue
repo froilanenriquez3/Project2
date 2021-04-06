4894f2437b13774ff49cbe1f629b5f4ca19a9310
Froilan-examen <fenriquez31@cepnet.net>
2021-04-06 13:26:57 +0200

    Create incidencies component

---
+++ broggi/resources/js/components/incidenciesComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <div class="form-group row">
+    <label class="col-2" for="">Numero de incidente</label>
+    <input class="col-10" type="number"  name="">
+</div>
+<div class="form-group row">
+    <label class="col-2" for="">Data</label>
+    <input class="col-10" type="date"  name="">
+</div>
+<div class="form-group row">
+    <label class="col-2" for="">Hora</label>
+    <input class="col-10" type="number"  name="">
+</div>
+<div class="form-group row">
+    <label class="col-2" for="">Telefon Alertant</label>
+    <input class="col-10" type="tel"  name="">
+</div>
+<div class="form-group row">
+    <label class="col-2" for="">Adreca</label>
+    <input class="col-10" type="tel"  name="">
+</div>
+<div class="form-group row">
+    <label class="col-2" for="">Adreca Complement</label>
+    <input class="col-10" type="tel"  name="">
+</div>
+<div class="form-group row">
+    <label class="col-2" for="">Descripcio</label>
+    <input class="col-10" type="tel"  name="">
+</div>
+<div class="form-group row">
+    <label class="col-2" for="">Nom Metge</label>
+    <input class="col-10" type="tel"  name="">
+</div>
+<div class="form-group row">
+    <label class="col-2" for="">Tipus Incidencia</label>
+    <select class="col-10" name=""  ></select>
+</div>
+<div class="form-group row">
+    <label class="col-2" for="">Alertant</label>
+    <input class="col-10" type="text"  name="">
+</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
