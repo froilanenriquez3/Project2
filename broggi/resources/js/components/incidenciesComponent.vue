bf2f57d77b3645248d2a99c5fb1ad630583e0e26
Froilan-examen <fenriquez31@cepnet.net>
2021-04-08 13:34:21 +0200

    Add v-model to form inputs incidencies

--- broggi/resources/js/components/incidenciesComponent.vue
+++ broggi/resources/js/components/incidenciesComponent.vue
@@ -1,46 +1,46 @@
 <template>
   <div class="container">
     <label class="" for="">Tipus Incidencia</label>
-    <select class="" name=""></select>
+    <select class="" name="" v-model="incidencia.tipus_incidencies_id">
+        <option value=""></option>
+    </select>
 
     <div class="form-group row">
       <label class="col-2" for="">Adreça</label>
-      <input class="col-10" type="tel" name="" />
+      <input class="col-10" type="tel" name="" v-model="incidencia.adreca"/>
     </div>
     <div class="form-group row">
       <label class="col-2" for="">Adreça Complement</label>
-      <input class="col-10" type="tel" name="" />
+      <input class="col-10" type="tel" name="" v-model="incidencia.adreca_complement"/>
     </div>
     <div>
       <label for="">Municipi</label>
-      <select name="" id="">
+      <select name="" id="" v-model="incidencia.municipis_id">
         <option value=""></option>
       </select>
     </div>
 
     <div class="form-group row">
       <label class="col-2" for="">Hora</label>
-      <input class="col-2" type="number" name="" />
+      <input class="col-2" type="number" name="" v-model="incidencia.hora"/>
 
       <label class="col-2" for="">Data</label>
-      <input class="col-2" type="date" name="" />
+      <input class="col-2" type="date" name="" v-model="incidencia.data"/>
     </div>
 
     <div class="form-group row">
       <label class="col-2" for="">Descripcio</label>
-      <textarea class="col-10" type="" name=""> </textarea>
+      <textarea class="col-10" type="" name="" v-model="incidencia.descripcio"> </textarea>
     </div>
 
     <div class="form-group row">
       <label class="col-2" for="">Nom Metge</label>
-      <input class="col-10" type="tel" name="" />
+      <input class="col-10" type="tel" name="" v-model="incidencia.nom_metge"/>
     </div>
 
-
-
     <!-- add fa plus icon -->
-    <button class="btn btn-primary" :click="addAlertantInput()">
-      Añadir alertante
+    <button class="btn btn-primary" :click="addAfectatInput()">
+      Añadir afectat
     </button>
   </div>
 </template>
@@ -53,7 +53,21 @@ export default {
     return {
       afectatFormComponent,
       alertantFormComponent,
-      incidencia: {},
+      incidencia: {
+            numIncident: null,
+            data: null,
+            hora: null,
+            telefon_alertant: null,
+            adreca: null,
+            adreca_complement: null,
+            descripcio: null,
+            nom_metge: null,
+            tipus_incidencies_id: null,
+            alertants_id: null,
+            municipis_id: null,
+            usuaris_id: null,
+            duracion: null
+      },
       incidencies: [],
     };
   },
@@ -87,7 +101,7 @@ export default {
           // me.errorMessage= error.response.data.error;
         });
     },
-    addAlertantInput() {
+    addAfectatInput() {
       //add limit for num alertants
       //let input = document.createElement("");
     },
